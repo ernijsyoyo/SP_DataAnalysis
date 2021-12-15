717e07a057659da8ab423a38595081cd4873bb30
ErnestsLavrinovicsalias] <ernests.lavrinovics@gmail.com>
2021-12-15 12:33:09 +0100

    Add constants of data directory locations

---
+++ Scripts/Constants.py
@@ -0,0 +1,12 @@
+import os
+
+def getAbsPath(relativePath):
+    output = os.path.abspath( os.path.join( os.path.dirname( __file__ ), relativePath) )
+    if (os.path.exists(output)):
+        return output
+    else:
+        raise Exception(f"Path <{relativePath}> relative to Constants.py does not exis")
+
+
+FOLDER_DATA_POSITIONS = getAbsPath("../Data/Positions")
+FOLDER_DATA_QUESTIONNAIRE = getAbsPath("../Data/Questionnaire")
---
+++ Scripts/NormalityTests.py
@@ -0,0 +1,5 @@
+from Constants import *
+
+""" Test the data for Gaussian distribution """
+print(FOLDER_DATA_POSITIONS)
+print(FOLDER_DATA_QUESTIONNAIRE)
